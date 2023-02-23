c77bc768aa5f778f6d818d60b56f336573fd314b
saugat330 <ssaugat349@gmail.com>
2023-02-23 10:33:03 +0545

    single array

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args){
+       // int[] x = new int[5];
+       // int[] y = {10,11,12};
+       int[] x ={1,2,3,4,5,6,7,8,9,0};
+
+       for(int qwert : x){
+
+        System.out.println(qwert);
+
+       }
+       
+    }
+    
+}
